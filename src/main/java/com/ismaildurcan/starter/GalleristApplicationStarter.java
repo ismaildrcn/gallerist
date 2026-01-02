6ef02fc157f495bd364d7f7d4b7edc687d414cc2
İsmail Durcan <ismaildurcan40@gmail.com>
2026-01-02 23:21:57 +0300

    Initial commit

---
+++ src/main/java/com/ismaildurcan/starter/GalleristApplicationStarter.java
@@ -0,0 +1,13 @@
+package com.ismaildurcan.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleristApplicationStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalleristApplicationStarter.class, args);
+	}
+
+}
